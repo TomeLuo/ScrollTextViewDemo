d4bea88a9c6cf787ad4bc24ceb5da03ed7177051
tongchuan <qwe6664321@163.com>
2018-06-01 16:34:20 +0800

    Initial Commit

---
+++ ScrollTextViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ScrollTextViewDemo
+//
+//  Created by 童川 on 2018/6/1.
+//  Copyright © 2018年 童川 QQ:731358928. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
